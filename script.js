0e6245e3d95c0cf194ccde5fbefd798466057c79
Shivansh2304 <72212391+Shivansh2304@users.noreply.github.com>
2023-03-31 15:59:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,140 @@
+class PhotoGallery{
+    constructor(){
+      this.API_KEY = 'qZWe2ZT3J0SLaIHHdfFUjxbvPXx3P5CmlOw92gfDQEgoDOn6WtJtoKPh';
+      this.galleryDIv = document.querySelector('.gallery');
+      this.searchForm = document.querySelector('.header form');
+      this.loadMore = document.querySelector('.load-more');
+      this.logo = document.querySelector('.logo')
+      this.pageIndex = 1;
+      this.searchValueGlobal = '';
+      this.eventHandle();
+    }
+    eventHandle(){
+      document.addEventListener('DOMContentLoaded',()=>{
+        this.getImg(1);
+      });
+      this.searchForm.addEventListener('submit', (e)=>{
+        this.pageIndex = 1;
+        this.getSearchedImages(e);
+      });
+      this.loadMore.addEventListener('click', (e)=>{
+        this.loadMoreImages(e);
+      })
+      this.logo.addEventListener('click',()=>{
+        this.pageIndex = 1;
+        this.galleryDIv.innerHTML = '';
+        this.getImg(this.pageIndex);
+      })
+    }
+    async getImg(index){
+      this.loadMore.setAttribute('data-img', 'curated');
+      const baseURL = `https://api.pexels.com/v1/curated?page=${index}&per_page=12`;
+      const data = await this.fetchImages(baseURL);
+      this.GenerateHTML(data.photos)
+      console.log(data)
+    }
+    async fetchImages(baseURL){
+      const response = await fetch(baseURL, {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          Authorization: this.API_KEY
+        }
+      });
+      const data = await response.json();
+      // console.log(data);
+      return data;
+    }
+
+    // GenerateHTML(photos){
+    //   photos.forEach(photo=>{
+    //     const item= document.createElement('div');
+    //     item.classList.add('item');
+    //     item.innerHTML = `
+    //     <a href='${photo.src.original}' target="_blank">
+    //       <img src="${photo.src.medium}">
+    //       <h3>${photo.photographer}</h3>
+    //     </a>
+    //     `;
+    //     this.galleryDIv.appendChild(item)
+    //   })
+    // }
+    //dfdgfnh
+    GenerateHTML(photos){
+      photos.forEach((photo, index)=>{
+        const item= document.createElement('div');
+        item.classList.add('item');
+        item.innerHTML = `
+          <img src="${photo.src.medium}" data-index="${index}">
+          <h3>${photo.photographer}</h3>
+        `;
+        this.galleryDIv.appendChild(item);
+
+        // Add click event listener to image
+        item.querySelector('img').addEventListener('click', () => {
+          const modal = document.createElement('div');
+          modal.classList.add('modal');
+          modal.innerHTML = `
+            <span class="close">&times;</span>
+            <img src="${photo.src.original}">
+            <div class="nav">
+              <button class="prev">&lt;</button>
+              <button class="next">&gt;</button>
+            </div>
+          `;
+          document.body.appendChild(modal);
+
+          // Set current image index
+          let currentIndex = index;
+
+          // Add click event listener to close button
+          modal.querySelector('.close').addEventListener('click', () => {
+            modal.remove();
+          });
+
+          // Add click event listeners to previous and next buttons
+          modal.querySelector('.prev').addEventListener('click', () => {
+            currentIndex = (currentIndex - 1 + photos.length) % photos.length;
+            modal.querySelector('img').src = photos[currentIndex].src.original;
+          });
+
+          modal.querySelector('.next').addEventListener('click', () => {
+            currentIndex = (currentIndex + 1) % photos.length;
+            modal.querySelector('img').src = photos[currentIndex].src.original;
+          });
+        });
+      });
+    }
+    //cdfgbf
+    async getSearchedImages(e){
+      this.loadMore.setAttribute('data-img', 'search');
+      e.preventDefault();
+      this.galleryDIv.innerHTML='';
+      const searchValue = e.target.querySelector('input').value;
+      this.searchValueGlobal = searchValue;
+      const baseURL = `https://api.pexels.com/v1/search?query=${searchValue}&page=1&per_page=12`
+      const data = await this.fetchImages(baseURL);
+      this.GenerateHTML(data.photos);
+      e.target.reset();
+    }
+    async getMoreSearchedImages(index){
+      // console.log(searchValue)
+      const baseURL = `https://api.pexels.com/v1/search?query=${this.searchValueGlobal}&page=${index}&per_page=12`
+      const data = await this.fetchImages(baseURL);
+      console.log(data)
+      this.GenerateHTML(data.photos);
+    }
+    loadMoreImages(e){
+      let index = ++this.pageIndex;
+      const loadMoreData = e.target.getAttribute('data-img');
+      if(loadMoreData === 'curated'){
+        // load next page for curated]
+        this.getImg(index)
+      }else{
+        // load next page for search
+        this.getMoreSearchedImages(index);
+      }
+    }
+  }
+
+  const gallery = new PhotoGallery;
